afa999096b72b44698e44be6a4e791f20add4b33
gzmtzly <gzmtzly@126.com>
2023-08-08 11:08:37 +0800

    Update Train_imbalanced_htru.py

--- Train_imbalanced_htru.py
+++ Train_imbalanced_htru.py
@@ -2,52 +2,52 @@
 # @Author  : Zhou-Lin-yong
 # @File    : Train_imbalanced_htru.py
 # @SoftWare: PyCharm
-import time, math, utils
+import time, math, Model, utils
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from sklearn.metrics import confusion_matrix, roc_auc_score, average_precision_score
 from collections import Counter
-import Loss_function
+import Loss_Function
 import sklearn.metrics as metrics
 import torch.nn.functional as F
 batch_size = 100
 
-abs_path = './result'
+abs_path = './Htru_result'
 
-Loss_fun = "CE"
+# Loss_fun = "CE"
 # Loss_fun = "FL"
 # Loss_fun = "ASL"
 # Loss_fun = "CL"
 # Loss_fun = "FTL"
 # Loss_fun = "HFL"
-# Loss_fun = 'GPPE'
+Loss_fun = 'GPPE'
 
 train_times = 1
 
 if Loss_fun == 'CE':
     Loss = nn.CrossEntropyLoss().cuda()
-    # save_confu_path = abs_path + '/result_CE/' + str(train_times) + '/'
+    save_confu_path = abs_path + '/result_CE/' + str(train_times) + '/'
     # save_confu_path = abs_path + '/result_CE_ROS/' + str(train_times) + '/'
-    save_confu_path = abs_path + '/result_CE_RUS/' + str(train_times) + '/'
+    # save_confu_path = abs_path + '/result_CE_RUS/' + str(train_times) + '/'
 elif Loss_fun == 'FL':
-    Loss = Loss_function_htru.Focal_Loss().cuda()
+    Loss = Loss_Function.Focal_Loss().cuda()
     save_confu_path = abs_path + '/result_FL/' + str(train_times) + '/'
 elif Loss_fun == 'ASL':
-    Loss = Loss_function_htru.ASLSingleLabel().cuda()
+    Loss = Loss_Function.ASLSingleLabel().cuda()
     save_confu_path = abs_path + '/result_ASL/' + str(train_times) + '/'
 elif Loss_fun == 'CL':
-    Loss = Loss_function_htru.Combo_Loss().cuda()
+    Loss = Loss_Function.Combo_Loss().cuda()
     save_confu_path = abs_path + '/result_CL/' + str(train_times) + '/'
 elif Loss_fun == 'FTL':
-    Loss = Loss_function_htru.Focal_Tversky_Loss_2().cuda()
+    Loss = Loss_Function.Focal_Tversky_Loss_2().cuda()
     save_confu_path = abs_path + '/result_FTL/' + str(train_times) + '/'
 elif Loss_fun == 'HFL':
-    Loss = Loss_function_htru.Hybrid_Focal_Loss().cuda()
+    Loss = Loss_Function.Hybrid_Focal_Loss().cuda()
     save_confu_path = abs_path + '/result_HFL/' + str(train_times) + '/'
 else:
-    Loss = Loss_function_htru.GPPE().cuda()
+    Loss = Loss_Function.GPPE(beta_neg=0).cuda()
     save_confu_path = abs_path + '/result_GPPE/' + str(train_times) + '/'
 
 
@@ -55,16 +55,14 @@ print('Loss:', Loss)
 print('save_confu_path:', save_confu_path)
 
 ### train data
-train_data_pulsar_path = np.load("./file_info_trans64/pulsar_path_trans64.npy")[:360]
-# train_data_pulsar_path = np.array(list(train_data_pulsar_path) * 75)
-train_data_rfi_path = np.load("./file_info_trans64/rfi_path_trans64.npy")[:27000]
-
+train_data_pulsar_path = np.load("./htru_file_info_trans64/pulsar_path_trans64.npy")[:360]
+train_data_rfi_path = np.load("./htru_file_info_trans64/rfi_path_trans64.npy")[:27000]
 print('train_data_pulsar_path:', len(train_data_pulsar_path))
 print('train_data_rfi_path:', len(train_data_rfi_path))
 
 ### test data
-test_data_pulsar_path = np.load("./file_info_trans64/pulsar_path_trans64.npy")[600:]
-test_data_rfi_path = np.load("./file_info_trans64/rfi_path_trans64.npy")[45000:]
+test_data_pulsar_path = np.load("./htru_file_info_trans64/pulsar_path_trans64.npy")[600:]
+test_data_rfi_path = np.load("./htru_file_info_trans64/rfi_path_trans64.npy")[45000:]
 print('test_data_pulsar_path:', len(test_data_pulsar_path))
 print('test_data_rfi_path:', len(test_data_rfi_path))
 
@@ -89,7 +87,7 @@ for i in train_data_path:
 
 train_data = np.array(train_data)
 train_label = np.array(train_label)
-print(Counter(train_label))
+print('train:', Counter(train_label))
 
 test_data = []
 test_label = []
@@ -100,7 +98,7 @@ for i in test_data_path:
 
 test_data = np.array(test_data)
 test_label = np.array(test_label)
-
+print('test:', Counter(test_label))
 
 train_data = train_data.reshape((train_data.shape[0], 1, 64, 64))
 test_data = test_data.reshape((test_data.shape[0], 1, 64, 64))
@@ -108,15 +106,15 @@ test_data = test_data.reshape((test_data.shape[0], 1, 64, 64))
 train_label = np.int32(train_label)
 test_label = np.int32(test_label)
 
-print('train_data:', train_data.shape)
-print('test_data:', test_data.shape)
+# print('train_data:', train_data.shape)
+# print('test_data:', test_data.shape)
 
 num_batch_train = math.ceil(train_data.shape[0] / batch_size)
 test_num_bathces = math.ceil(test_data.shape[0] / batch_size)
 
-CONV = Model_htru.Conv().cuda()
+CONV = Model.Model_Ttru().cuda()
 
-optimer = torch.optim.Adam(CONV.parameters(), lr=0.5, betas=(0.5, 0.999))   # FTL在lr=0.0001下才能训练出结果
+optimer = torch.optim.Adam(CONV.parameters(), lr=0.02, betas=(0.5, 0.999))   
 
 print('---------- Networks architecture -------------')
 utils.print_network(CONV)
@@ -132,7 +130,6 @@ for epoch in range(EPOCH):
 
         data = train_data[from_l_c:to_l_c]
         label = train_label[from_l_c:to_l_c]
-        # print('label:', Counter(label))
 
         data = Variable(torch.from_numpy(data).float()).cuda()
         label = Variable(torch.from_numpy(label).long()).cuda()
@@ -176,15 +173,11 @@ for epoch in range(EPOCH):
     precision = metrics.precision_score(ground_truth_valid, label_valid)
     recall = metrics.recall_score(ground_truth_valid, label_valid)
     f_score = metrics.f1_score(ground_truth_valid, label_valid)
-    AUC = roc_auc_score(ground_truth_valid, probility_predicted[:, -1])
-    AP = average_precision_score(ground_truth_valid, probility_predicted[:, -1])
 
     print(Confu_matir)
     print('Precision:', precision)
     print('Recall:', recall)
     print('F_score:', f_score)
-    print('AUC:', AUC)
-    print('AP:', AP)
 
     if epoch > EPOCH - 20:
         np.save(save_confu_path + 'Confu_matir_' + str(epoch) + '.npy', Confu_matir)
